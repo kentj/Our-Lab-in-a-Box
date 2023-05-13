4a7e1a8e488e428ec341959c094f2bd6c2dbc019
John Kent <jkent@mitre.org>
2023-05-13 07:24:47 -0400

    Updating templates and script file to correct variable replacements.

--- scripts/script.py
+++ scripts/script.py
@@ -1,6 +1,7 @@
 import os
 import urllib.request
 import ssl
+from string import Template
 
 def getInput(prompt):
    print("\n\n")
@@ -27,6 +28,7 @@ def getURLContent(URL):
 
 def writeFileContent(fileName, data_to_write):
    print(f"Writing file: {fileName}")
+   print(f"Data: {data_to_write}")
    outputFile = open(fileName, 'w')
    outputFile.write(data_to_write)
    outputFile.close()
@@ -35,8 +37,9 @@ def writeFileContent(fileName, data_to_write):
 def transformTemplate(variables, template_URL, fileName):
    print(f"Template: {template_URL}",f"File: {fileName}")
    try:
-      template_data = getURLContent(templateURL)
-      #template_data
+      template_data = getURLContent(templateURL).decode('utf-8')
+      template = Template(template_data)
+      completed_template_data = template.substitute(variables)
       writeFileContent(fileName, completed_template_data)
    except Exception as e:
       print(e)
@@ -44,96 +47,104 @@ def transformTemplate(variables, template_URL, fileName):
    
    return True
 
-labAbbreviation = getInput("Lab abbreviation:")
-variables = {}
-variables['labAbbreviation']=labAbbreviation
+variables = {'PWD' : '$PWD'}
+
+labAbbreviation = getInput("Lab abbreviation: ")
+variables['labAbbreviation'] = labAbbreviation.lower()
+
+fqdnEnding = getInput("Final segment of lab's FQDN (org, test, etc): ")
+variables['fqdnEnding'] = fqdnEnding
 
 print("Lab-in-a-Box Setup and Configuration Tool v0.1")
 print("----------------------------------------------\n")
 print("Summary of your inputs and calculated values: ")
 print(f"01: Lab Abbreviation: {labAbbreviation}")
 
-labName = labAbbreviation.lower() + "-lab"
-variables['labName']=labName
-print(f"02: Lab Name: {labName}")
-
+labName = labAbbreviation + "-lab"
+labNameUnderscoredUpper = (labAbbreviation + "_lab").upper()
+variables['labName'] = labName
+variables['labNameUnderscoredUpper'] = labNameUnderscoredUpper
+print(f"02a: Lab Name: {labName}")
+print(f"02b: Lab Name UnderScore Uppercase: {labNameUnderscoredUpper}")
+print(f"03: Lab FQDN ending: {fqdnEnding}")
 rootCADirectory = labAbbreviation.upper() + "-Lab-CA"
-variables['rootCADirectory']=rootCADirectory
+variables['rootCADirectory'] = rootCADirectory
 rootCA_certificate_file = rootCADirectory + '.crt'
-variables['rootCA_certificate_file']=rootCA_certificate_file
+variables['rootCA_certificate_file'] = rootCA_certificate_file
 rootCA_certificate_key_file = rootCADirectory + ".key"
-variables['rootCA_certificate_key_file']=rootCA_certificate_key_file
+variables['rootCA_certificate_key_file'] = rootCA_certificate_key_file
 
-print(f"03: Lab Root Certificate Authority Directory: {rootCADirectory}")
-print(f"04. Lab Root CA Certificate File: {rootCA_certificate_file}")
-print(f"05. Lab Root CA Certificate Key File: {rootCA_certificate_key_file}")
+print(f"04: Lab Root Certificate Authority Directory: {rootCADirectory}")
+print(f"05. Lab Root CA Certificate File: {rootCA_certificate_file}")
+print(f"06. Lab Root CA Certificate Key File: {rootCA_certificate_key_file}")
 
-lab_base_URL = f"{labName}.nccoe.test"
-variables['lab_base_URL']=lab_base_URL
-print(f"06. Lab Base URL: {lab_base_URL}")
+lab_base_URL = f"{labName}.nccoe.{fqdnEnding}"
+variables['lab_base_URL'] = lab_base_URL
+print(f"07. Lab Base URL: {lab_base_URL}")
 
 certificate_file_suffix = lab_base_URL + ".crt"
-variables['certificate_file_suffix']=certificate_file_suffix
+variables['certificate_file_suffix'] = certificate_file_suffix
 certificate_key_file_suffix = lab_base_URL + ".key"
-variables['certificate_key_file_suffix']=certificate_key_file_suffix
+variables['certificate_key_file_suffix'] = certificate_key_file_suffix
 
 guacamole_certificate_file = 'guacamole.' + certificate_file_suffix
-variables['guacamole_certificate_file']=guacamole_certificate_file
+variables['guacamole_certificate_file'] = guacamole_certificate_file
 guacamole_certificate_key_file = 'guacamole.' + certificate_key_file_suffix
-variables['guacamole_certificate_key_file']=guacamole_certificate_key_file
-print(f"07. Guacamole Certificate File: {guacamole_certificate_file}")
-print(f"08. Guacamole Certificate Key File: {guacamole_certificate_key_file}")
+variables['guacamole_certificate_key_file'] = guacamole_certificate_key_file
+print(f"08. Guacamole Certificate File: {guacamole_certificate_file}")
+print(f"09. Guacamole Certificate Key File: {guacamole_certificate_key_file}")
 
 keycloak_certificate_file = 'keycloak.' + certificate_file_suffix
-variables['keycloak_certificate_file']=keycloak_certificate_file
+variables['keycloak_certificate_file'] = keycloak_certificate_file
 keycloak_certificate_key_file = 'keycloak.' + certificate_key_file_suffix
-variables['keycloak_certificate_key_file']=keycloak_certificate_key_file
-print(f"09. Keycloak Certificate File: {keycloak_certificate_file}")
-print(f"10. Keycloak Certificate Key File: {keycloak_certificate_key_file}")
+variables['keycloak_certificate_key_file'] = keycloak_certificate_key_file
+print(f"10. Keycloak Certificate File: {keycloak_certificate_file}")
+print(f"11. Keycloak Certificate Key File: {keycloak_certificate_key_file}")
 
 minio_certificate_file = 'minio.' + certificate_file_suffix
-variables['minio_certificate_file']=minio_certificate_file
+variables['minio_certificate_file'] = minio_certificate_file
 minio_certificate_key_file = 'minio.' + certificate_key_file_suffix
-variables['minio_certificate_key_file']=minio_certificate_key_file
-print(f"11. MinIO Certificate File: {minio_certificate_file}")
-print(f"12. MinIO Certificate Key File: {minio_certificate_key_file}")
+variables['minio_certificate_key_file'] = minio_certificate_key_file
+print(f"12. MinIO Certificate File: {minio_certificate_file}")
+print(f"13. MinIO Certificate Key File: {minio_certificate_key_file}")
 
 remote_certificate_file = 'remote.' + certificate_file_suffix
-variables['remote_certificate_file']=remote_certificate_file
+variables['remote_certificate_file'] = remote_certificate_file
 remote_certificate_key_file = 'remote.' + certificate_key_file_suffix
-variables['remote_certificate_key_file']=remote_certificate_key_file
-print(f"13. Remote Certificate File: {remote_certificate_file}")
-print(f"14. Remote Certificate Key File: {remote_certificate_key_file}")
+variables['remote_certificate_key_file'] = remote_certificate_key_file
+print(f"14. Remote Certificate File: {remote_certificate_file}")
+print(f"15. Remote Certificate Key File: {remote_certificate_key_file}")
 
-
+print("\nMaking system changes...\n")
 print("Step 1:  Creating directories...")
 
-for x in [ '/home/administrator/guacamole','/home/administrator/guacamole/caddy',
-           '/home/administrator/guacamole/caddy/config',
-           '/home/administrator/guacamole/caddy/config',
-           '/home/administrator/guacamole/caddy/data',
-           f'/home/administrator/guacamole/certs/{rootCADirectory}',
-           '/home/administrator/guacamole/certs/guacamole',
-           '/home/administrator/guacamole/certs/keycloak',
-           '/home/administrator/guacamole/certs/minio',
-           '/home/administrator/guacamole/certs/remote',
-           '/home/administrator/guacamole/guacamole_build',
-           '/home/administrator/guacamole/guacdb/databases',
-           '/home/administrator/guacamole/guacdb/init',
-           '/home/administrator/guacamole/keycloakdb/databases' ]:
+directory_base='./home/administrator/'
+for x in [ 'guacamole','guacamole/caddy',
+           'guacamole/caddy/config',
+           'guacamole/caddy/data',
+           f'guacamole/certs/{rootCADirectory}',
+           'guacamole/certs/guacamole',
+           'guacamole/certs/keycloak',
+           'guacamole/certs/minio',
+           'guacamole/certs/remote',
+           'guacamole/guacamole_build',
+           'guacamole/guacdb/databases',
+           'guacamole/guacdb/init',
+           'guacamole/keycloakdb/databases'
+         ]:
+   directory = directory_base + x
+   print(f'.... Creating directory{directory}')
+   os.makedirs(directory)
 
-   print(f'.... Creating directory{x}')
-   #os.makedirs(x)
+print("\nStep 2:  Creating files from templates...\n")
 
-print("Step 2:  Creating files from templates.")
-
-baseURL = "https://raw.githubusercontent.com/kentj/NCCoE-Lab-in-a-Box/main/templates/"
+baseURL = "https://raw.githubusercontent.com/kentj/Our-Lab-in-a-Box/main/templates/"
 templateSuffix = '.template'
-baseLocation = "/home/administrator/guacamole/"
+baseLocation = "./home/administrator/guacamole/"
 
 for x in [ ['Caddyfile', 'caddy/Caddfile'],
            ['Dockerfile', 'guacamole_build/Dockerfile'],
-           ['.env', '.env'],
+           ['env', '.env'],
            ['caddy_reload.sh', 'caddy_reload.sh'],
            ['docker-compose.yaml.initializeOpenLDAP', 'docker-compose.yaml.initializeOpenLDAP'],
            ['docker-compose.yaml', 'docker-compose.yaml'] ]:
@@ -142,9 +153,9 @@ for x in [ ['Caddyfile', 'caddy/Caddfile'],
    fileName = baseLocation + x[1]
    result = transformTemplate(variables, templateURL, fileName)
    if result == True:
-      print(f"Completion of template for {x[0]} sucessful!")
+      print(f"Completion of template for {x[0]} sucessful!\n")
    else:
-      print("Failed...\n{result}")
+      print(f"Failed...\n{result}\n")
 
    
 
